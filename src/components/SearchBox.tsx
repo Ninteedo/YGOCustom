6777535d28f9c8dfe09fec9f2740f96dbf294bb6
Richard Holmes <richard.gordon.holmes@gmail.com>
2024-05-26 16:18:03 +0100

    Simple search by ID

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,10 +1,18 @@
-import {ReactNode, useEffect} from "react";
+import React, {ReactNode, useEffect, useState} from "react";
 import "../style/SearchBox.scss";
+import {BaseCard} from "./card/abstract/BaseCard.ts";
+import CardSmall from "./card/display/CardSmall.tsx";
+import cardManifest from "../cardManifest.json";
+import {loadCard} from "./card/abstract/CardLoader.tsx";
+import {useNavigate} from "react-router-dom";
 
 export default function SearchBox({isVisible, toggleSearch}: {
   isVisible: boolean,
   toggleSearch: () => void
 }): ReactNode {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchResults, setSearchResults] = useState<BaseCard[]>([]);
+
   useEffect(() => {
     const body = document.querySelector("body") as HTMLElement;
     if (isVisible) {
@@ -14,17 +22,69 @@ export default function SearchBox({isVisible, toggleSearch}: {
     }
   }, [isVisible]);
 
+  useEffect(() => {
+
+    // setSearchResults(searchCards(searchTerm));
+    const fetchResults = async () => {
+      const results = await searchCards(searchTerm);
+      setSearchResults(results);
+    }
+    fetchResults();
+  }, [searchTerm]);
+
+  const handleInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  }
+
   return (
     <>
       <div className={"search-box " + (isVisible ? "visible" : "hidden")}>
         <div>
-          <input type="text" name={"search-term"} className={"search-term"} placeholder={"Search"}/>
+          <input type="text" name={"search-term"} className={"search-term"} placeholder={"Search"}
+                 onInput={handleInput}/>
           <button className={"close-button"} onClick={toggleSearch}>x</button>
         </div>
-        <div className={"results"}>
-        </div>
+        <SearchResults results={searchResults} toggleSearch={toggleSearch}/>
       </div>
       <div className={"search-box-overlay" + (isVisible ? "" : " hidden")} onClick={toggleSearch}/>
     </>
   )
+}
+
+function SearchResults({results, toggleSearch}: { results: BaseCard[], toggleSearch: () => void }) {
+  return (
+    <div className={"results"}>
+      {results.map((result, index) => {
+        return <SearchResult key={index} card={result} toggleSearch={toggleSearch}/>
+      })}
+    </div>
+  )
+}
+
+function SearchResult({card, toggleSearch}: { card: BaseCard, toggleSearch: () => void }) {
+  const navigate = useNavigate();
+
+  const clickAction = (id: string) => {
+    navigate(`/card/custom/${id}`);
+    toggleSearch();
+  }
+
+  return <CardSmall id={card.id} name={card.name} art={card.art} cardKind={card.kind} cardSubKind={card.subKind}
+                    clickAction={clickAction}/>
+}
+
+async function searchCards(searchTerm: string): Promise<BaseCard[]> {
+  const hits: string[] = cardManifest.entries.filter((entry: string) => {
+    return entry.toLowerCase().includes(searchTerm.toLowerCase());
+  });
+  const results: BaseCard[] = [];
+
+  for (const hit of hits) {
+    const card = await loadCard(hit);
+    if (card) {
+      results.push(card);
+    }
+  }
+
+  return results;
 }
---
+++ src/components/card/display/CardSmall.tsx
@@ -0,0 +1,45 @@
+import {CardArt} from "../CardArt.tsx";
+import "../../../style/CardSmall.scss";
+
+interface CardSmallProps {
+  id: string;
+  name: string;
+  art: string;
+  cardKind: string;
+  cardSubKind: string;
+  clickAction?: (id: string) => void;
+}
+
+export default function CardSmall({id, name, art, cardKind, cardSubKind, clickAction}: CardSmallProps) {
+  const handleClick = () => {
+    if (clickAction) {
+      clickAction(id);
+    }
+  }
+
+  let rootClasses = ["card", "card-small", `card-${cardKind}`, `card-${cardSubKind}`];
+  if (clickAction) {
+    rootClasses.push("has-click-action");
+  }
+
+  return (
+    <div className={rootClasses.join(" ")} data-card-id={id} onClick={handleClick}>
+      <div className={"card-content"}>
+        <SmallCardName name={name}/>
+        <SmallCardArt name={name} art={art}/>
+      </div>
+    </div>
+  )
+}
+
+function SmallCardName({name}: { name: string }) {
+  return (
+    <div className={"card-name"}>
+      {name}
+    </div>
+  )
+}
+
+function SmallCardArt({name, art}: { name: string, art: string }) {
+  return <CardArt src={art} alt={`Art for ${name}`}/>
+}
