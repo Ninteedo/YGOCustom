b7bb7835824b6cdd08d6da41b44a29f9be27c96b
Richard Holmes <richard.gordon.holmes@gmail.com>
2024-05-27 00:12:08 +0100

    Add prebuild script that queries data from aggregate file then saves DB for each language

---
+++ scripts/cardDbUpdate.ts
@@ -0,0 +1,65 @@
+import {exec} from 'child_process';
+import fs from "fs";
+// import cardDbTemp from "../scripts/cards.json";
+
+function gitSubmoduleUpdate(): void {
+  exec('git submodule update --init --recursive --depth 1', (err, stdout, stderr) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(stdout);
+    console.error(stderr);
+  });
+}
+
+async function loadCardDb(): Promise<any[]> {
+  return await fetch("https://dawnbrandbots.github.io/yaml-yugi/cards.json")
+    .then(response => response.json())
+    .then(data => {
+      console.log('Aggregate card data loaded.');
+      return data;
+    });
+
+  // return cardDbTemp;
+}
+
+function saveDbByLanguages(cardDb: any[]): void {
+  const LANGUAGES = ['en', 'de', 'es', 'fr', 'it', 'pt', 'ja', 'ko', 'zh-TW', 'zh-CN'];
+
+  for (const language of LANGUAGES) {
+    const db = getCardDbByLanguage(cardDb, language);
+    console.log(`Saving card data for ${language}.`)
+    fs.writeFileSync(`public/db/cards.${language}.json`, JSON.stringify(db, null, 2), 'utf8');
+    console.log(`Saved public/db/cards.${language}.json`);
+  }
+}
+
+function getCardDbByLanguage(cardDb: any[], language: string): any[] {
+  return cardDb.map(card => getCardByLanguage(card, language));
+}
+
+function getCardByLanguage(card: any, language: string): any {
+  const data = {...card};
+
+  // remove language fields
+  try {
+    data.name = data.name[language];
+    data.text = data.text[language];
+    data.sets = data.sets[language];
+  } catch (e) {
+    console.error(`Error processing card ${data.name} for language ${language}.`);
+    console.error(e);
+    throw e;
+  }
+
+  return data;
+}
+
+async function main(): Promise<void> {
+  gitSubmoduleUpdate();
+  const cardDb = await loadCardDb();
+  saveDbByLanguages(cardDb);
+}
+
+await main();
