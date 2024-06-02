15ac0f4d34688cc7fd732d46c7b0aab4d2bac2bf
Richard Holmes <richard.gordon.holmes@gmail.com>
2024-06-02 10:35:15 +0100

    Add stat line, info line, and category line to BaseDbCard

--- src/components/card/abstract/BaseDbCard.tsx
+++ src/components/card/abstract/BaseDbCard.tsx
@@ -1,51 +1,32 @@
 import {BaseCard} from "./BaseCard.ts";
-import React, {Fragment} from "react";
+import React, {Fragment, ReactNode} from "react";
 import CardTemplate from "./display/elements/CardTemplate.tsx";
+import StatLine from "./display/elements/StatLine.tsx";
 
 export default class BaseDbCard extends BaseCard {
   public readonly text: string;
+
+  private readonly json: any;
 
   constructor(json: any) {
     const id = json.konami_id;
     const name = json.name;
     const artSrc = getYgoProDeckImageLink(json.password);
-    const card_type = json.card_type;
-    const sub_kind = "not implemented yet";
+    const cardKind = json.card_type.toLowerCase();
+    const subKind = getDbCardSubKind(json).toLowerCase();
 
-    super(id, name, artSrc, card_type, sub_kind);
+    super(id, name, artSrc, cardKind, subKind);
 
-    console.log(json.text);
     this.text = json.text;
+    this.json = json;
   }
 
   toCardDetail(): React.ReactNode {
-    return (
-      <CardTemplate
-        id={this.id}
-        name={this.name}
-        artSrc={this.art}
-        infoLine={<p>Not implemented</p>}
-        effectBlock={<p>{this.text.split("\n").map(s => <Fragment>{s}<br/></Fragment>)}</p>}
-        cardKind={this.kind}
-        cardSubKind={this.subKind}
-        overrideArtSrc={true}
-      />
-    );
+    return this.toElement();
   }
 
   toCardElement(): React.ReactNode {
-    return (
-      <CardTemplate
-        id={this.id}
-        name={this.name}
-        artSrc={this.art}
-        infoLine={<p>Not implemented</p>}
-        effectBlock={<p>{this.text}</p>}
-        cardKind={this.kind}
-        cardSubKind={this.subKind}
-        overrideArtSrc={true}
-      />
-    );
+    return this.toElement();
   }
 
   toText(): string {
@@ -55,6 +36,60 @@ export default class BaseDbCard extends BaseCard {
   getLinkUrl(): string {
     return `/card/official/${this.id}`
   }
+
+  protected getInfoLine(): ReactNode {
+    switch (this.kind) {
+      case "monster":
+        const levelName = getLevelName(this.subKind, this.json);
+        const attribute = this.json.attribute;
+        return <p>{levelName} {attribute} Monster</p>
+      case "spell":
+        return <p>{this.subKind} Spell</p>
+      case "trap":
+        return <p>{this.subKind} Trap</p>
+      default:
+        return <p>Unknown</p>
+    }
+  }
+
+  protected getCategoryLine(): ReactNode {
+    if (this.kind === "monster") {
+      return <p>[{this.json.monster_type_line}]</p>;
+    } else {
+      return undefined;
+    }
+  }
+
+  protected getStatLine(): ReactNode {
+    if (this.kind === "monster") {
+      const atk = this.json.atk;
+      let def = 0;
+      if (this.json.def) {
+        def = this.json.def;
+      }
+
+      return <StatLine atk={atk} def={def}/>
+    } else {
+      return undefined;
+    }
+  }
+
+  protected toElement(): ReactNode {
+    return (
+      <CardTemplate
+        id={this.id}
+        name={this.name}
+        artSrc={this.art}
+        infoLine={this.getInfoLine()}
+        effectBlock={<p>{this.text.split("\n").map((s, index) => <Fragment key={index}>{s}<br/></Fragment>)}</p>}
+        cardKind={this.kind}
+        cardSubKind={this.subKind}
+        overrideArtSrc={true}
+        statLine={this.getStatLine()}
+        categoryLine={this.getCategoryLine()}
+      />
+    );
+  }
 }
 
 function getYugipediaLink(fileName: string): string {
@@ -63,4 +98,44 @@ function getYugipediaLink(fileName: string): string {
 
 function getYgoProDeckImageLink(passcode: string): string {
   return `https://images.ygoprodeck.com/images/cards_cropped/${passcode}.jpg?utm_source=rghdev`
+}
+
+function getDbCardSubKind(json: any): string {
+  switch (json.card_type.toLowerCase()) {
+    case "monster":
+      const monsterTypeLine = json.monster_type_line;
+      if (monsterTypeLine.includes("Fusion")) {
+        return "Fusion";
+      } else if (monsterTypeLine.includes("Synchro")) {
+        return "Synchro";
+      } else if (monsterTypeLine.includes("Xyz")) {
+        return "Xyz";
+      } else if (monsterTypeLine.includes("Link")) {
+        return "Link";
+      } else if (monsterTypeLine.includes("Ritual")) {
+        return "Ritual";
+      } else if (monsterTypeLine.includes("Effect")) {
+        return "Regular";
+      } else {
+        return "Normal";
+      }
+    case "spell":
+      return json.property.toLowerCase();
+    case "trap":
+      return json.property.toLowerCase();
+    default:
+      return "Unknown";
+  }
+}
+
+function getLevelName(monsterSubKind: string, json: any): string {
+  if (monsterSubKind === "link") {
+    const linkArrows = json.link_arrows;
+    const linkRating = linkArrows.length;
+    return `Link-${linkRating}`;
+  } else if (monsterSubKind === "xyz") {
+    return `Rank ${json.rank}`;
+  } else {
+    return `Level ${json.level}`;
+  }
 }
