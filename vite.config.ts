38593b617a3dfda20a9b73e1a82abd2948770360
Richard Holmes <richard.gordon.holmes@gmail.com>
2024-06-14 22:43:05 +0100

    Separate production env for IMAGE_BASE_URL

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,27 @@
-import {defineConfig} from "vite"
+import {defineConfig, loadEnv} from "vite"
 import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    watch: {
-      ignored: ["**/public/images/official/**"], // Exclude the public directory from being watched
-    },
-    proxy: {
-      "/image_bucket/": {
-        target: "https://images.ygo.rgh.dev",
-        changeOrigin: true,
-        secure: true,
-        rewrite: path => path.replace(/^\/image_bucket/, ""),
-        headers: {
-          // Set the referer to your actual domain to bypass the hotlinking restriction
-          Referer: "https://ygo.rgh.dev",
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode, process.cwd(), "VITE_");
+
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/image_bucket/": {
+          target: "https://images.ygo.rgh.dev",
+          changeOrigin: true,
+          secure: true,
+          rewrite: path => path.replace(/^\/image_bucket/, ""),
+          headers: {
+            Referer: "https://ygo.rgh.dev",
+          },
         },
       },
     },
-  },
-  define: {
-    "process.env.IMAGE_BASE_URL": "\"/image_bucket\"",
-  }
-})
+    define: {
+      "process.env.IMAGE_BASE_URL": JSON.stringify(env.VITE_IMAGE_BASE_URL),
+    },
+  };
+});
