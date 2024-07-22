10901efea526ce9ab4af9b8b826d592e4b8eff2c
Richard Holmes <richard.gordon.holmes@gmail.com>
2024-07-22 11:34:43 +0100

    Compress card race entries

--- src/dbCompression.ts
+++ src/dbCompression.ts
@@ -52,7 +52,6 @@ export class CardJsonEntry {
     this.desc = getMapping(json, "desc", isCompressed);
     this.atk = getMappingSafe(json, "atk", isCompressed);
     this.def = getMappingSafe(json, "def", isCompressed);
-    this.race = getMappingSafe(json, "race", isCompressed);
     this.attribute = getMappingSafe(json, "attribute", isCompressed);
     this.archetype = getMappingSafe(json, "archetype", isCompressed);
     this.level = getMappingSafe(json, "level", isCompressed);
@@ -60,8 +59,13 @@ export class CardJsonEntry {
 
     if (isCompressed) {
       this.imageId = getMapping(json, "imageId", isCompressed);
+      const raceNumberString = getMappingSafe(json, "race", isCompressed);
+      if (raceNumberString !== undefined) {
+        this.race = raceNumberToName(parseInt(raceNumberString));
+      }
     } else {
       this.imageId = json["card_images"][0]["id"];
+      this.race = getMappingSafe(json, "race", isCompressed);
     }
     // this.cardSets = (json[getMapping("card_sets", isCompressed)]; as Object[]).map(setJson => new CardSet(setJson));
   }
@@ -72,6 +76,10 @@ export class CardJsonEntry {
     for (const [key, compressedKey] of mappings) {
       // @ts-ignore
       compressed[compressedKey] = this[key];
+    }
+
+    if (this.race) {
+      compressed["r"] = raceNameToNumber(this.race);
     }
 
     return compressed;
@@ -98,6 +106,59 @@ function getMappingSafe(json: any, key: string, isCompressed: boolean): string |
     }
   }
   return undefined;
+}
+
+const raceMappings: [string, number][] = [
+  ["Normal", 7],
+  ["Quick-Play", 1],
+  ["Continuous", 2],
+  ["Counter", 3],
+  ["Ritual", 4],
+  ["Equip", 5],
+  ["Field", 6],
+  ["Pyro", 8],
+  ["Rock", 9],
+  ["Winged Beast", 10],
+  ["Plant", 11],
+  ["Insect", 12],
+  ["Thunder", 13],
+  ["Dragon", 14],
+  ["Beast", 15],
+  ["Beast-Warrior", 16],
+  ["Dinosaur", 17],
+  ["Fish", 18],
+  ["Sea Serpent", 19],
+  ["Reptile", 20],
+  ["Psychic", 21],
+  ["Divine-Beast", 22],
+  ["Creator-God", 23],
+  ["Wyrm", 24],
+  ["Cyberse", 25],
+  ["Illusion", 26],
+  ["Warrior", 27],
+  ["Spellcaster", 28],
+  ["Fairy", 29],
+  ["Fiend", 30],
+  ["Zombie", 31],
+  ["Machine", 32],
+  ["Aqua", 33],
+];
+
+
+function raceNameToNumber(raceName: string): number {
+  const found = raceMappings.find(([name, _]) => name === raceName);
+  if (found) {
+    return found[1];
+  }
+  throw new Error(`Race ${raceName} not found`);
+}
+
+function raceNumberToName(raceNumber: number): string {
+  const found = raceMappings.find(([_, number]) => number === raceNumber);
+  if (found) {
+    return found[0];
+  }
+  throw new Error(`Race number ${raceNumber} not found`);
 }
 
 // class CardSet {
