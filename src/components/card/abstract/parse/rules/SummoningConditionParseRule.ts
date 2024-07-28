68299c195f84c956cdf8a61ba7804b754423e176
Richard Holmes <richard.gordon.holmes@gmail.com>
2024-07-28 19:22:23 +0100

    Refactor SummoningConditionParseRule

--- src/components/card/abstract/parse/rules/SummoningConditionParseRule.ts
+++ src/components/card/abstract/parse/rules/SummoningConditionParseRule.ts
@@ -8,21 +8,20 @@ import EffectMainClause from "../../effect/clause/EffectMainClause.ts";
  */
 export default class SummoningConditionParseRule extends EffectParseRule {
   match({sentence, isSpellTrap}: EffectParseProps): boolean {
-    if (isSpellTrap) {
+    if (isSpellTrap || this.hasCost(sentence) || this.hasCondition(sentence)) {
       return false;
     }
 
-    if (this.hasCost(sentence) || this.hasCondition(sentence)) {
-      return false;
+    if (sentence.startsWith("Cannot be Normal Summoned/Set") || sentence.startsWith("Cannot be Special Summoned")) {
+      return true;
     }
 
     const summoningTypes = ["Ritual", "Fusion", "Synchro", "Xyz", "Link", "Special", "Tribute", "Normal"];
-    const positivePattern = "you can (also )?(" + summoningTypes.join("|") + ") summon (this card|\")";
-    const negativePattern = "cannot be (" + summoningTypes.join("|") + ") summoned";
+    const summoningTypeDisjunction = "(" + summoningTypes.join("|") + ")";
+    const positivePattern = `you can (also )?${summoningTypeDisjunction} summon (this card|")`;
+    const negativePattern = `cannot be ${summoningTypeDisjunction} summoned`;
     return !!(
-      sentence.startsWith("Cannot be Normal Summoned/Set")
-      || sentence.startsWith("Cannot be Special Summoned")
-      || sentence.match(/^Must (first )?be (\w)+ Summoned/)
+      sentence.match(/^Must (first )?be (\w)+ Summoned/)
       || sentence.match(new RegExp(positivePattern, "i"))
       || sentence.match(new RegExp(negativePattern, "i"))
     );
