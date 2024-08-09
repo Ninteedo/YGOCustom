d9c039331b53c8befdbd76fb0ba94e4e147d00de
Richard Holmes <richard.gordon.holmes@gmail.com>
2024-08-09 22:35:39 +0100

    Fix flip effects missing the FLIP clause

--- src/components/card/abstract/effect/FlipEffect.tsx
+++ src/components/card/abstract/effect/FlipEffect.tsx
@@ -1,18 +1,15 @@
 import TriggerEffect from "./TriggerEffect.tsx";
 import EffectClause from "./clause/EffectClause.ts";
 import {ReactNode} from "react";
+import EffectConditionClause from "./clause/EffectConditionClause.ts";
 
 export default class FlipEffect extends TriggerEffect {
   constructor(clauses: EffectClause[]) {
-    super(clauses);
+    super([new EffectConditionClause("FLIP"), ...clauses]);
   }
 
   public render(): ReactNode {
-    return (
-      <>
-        {super.render()}
-      </>
-    )
+    return super.render();
   }
 
   public addSubEffect(clauses: EffectClause[]): void {
