7aadd92e03dae53cec5276f368c3b7a349c2768a
Richard Holmes <richard.gordon.holmes@gmail.com>
2024-08-31 10:33:49 +0100

    Add summon icon to summoning conditions

--- src/components/card/abstract/effect/SummoningCondition.tsx
+++ src/components/card/abstract/effect/SummoningCondition.tsx
@@ -1,6 +1,7 @@
 import Effect from "./Effect.tsx";
 import EffectClause from "./clause/EffectClause.ts";
 import {ReactNode} from "react";
+import SpecialSummonIcon from "../../../../assets/images/properties/SpecialSummon.svg";
 
 export default class SummoningCondition extends Effect {
   public readonly clause: EffectClause;
@@ -13,7 +14,7 @@ export default class SummoningCondition extends Effect {
   public render(): ReactNode {
     return (
       <>
-        {/*<img className={"inline-logo"} src={ContinuousIcon} alt={"Continuous Icon"} title={"Continuous Effect"}/>*/}
+        <img className={"inline-logo"} src={SpecialSummonIcon} alt={"Continuous Icon"} title={"Continuous Effect"}/>
         <span className={"summoning-condition"}>{this.clause.render()}</span>
       </>
     )
